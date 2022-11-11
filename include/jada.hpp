febe374f5fc431b7bca9623dd8763435feb7bb47
petteri <petteri.peltonen@vtt.fi>
2022-11-11 11:18:43 +0200

    first

---
+++ include/jada.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "bits/mdspan.hpp"
+#include "bits/subspan.hpp"
+#include "bits/extents.hpp"
+#include "bits/decomposition.hpp"
+#include "bits/loop.hpp"
+#include "bits/neighbours.hpp"
+#include "bits/tiled_stencil.hpp"
+#include "bits/evaluate_tiled.hpp"
+#include "bits/rank.hpp"
